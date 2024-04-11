4590382ba452f7981d1c0eaab3fc767cf087aa0c
张伟峰 <zhang.weifeng@tslsmart.com>
2024-04-11 17:33:33 +0800

    feat: 增加下落效果

--- src/shaders/fireworks/vertex.glsl
+++ src/shaders/fireworks/vertex.glsl
@@ -5,20 +5,29 @@ uniform float uProgress;
 
 attribute float aDelay;
 
+float remap(float value, float originMin, float originMax, float destinationMin, float destinationMax)
+{
+    return destinationMin + (value - originMin) * (destinationMax - destinationMin) / (originMax - originMin);
+}
+
 void main() {
     vec3 newPosition = position;
 
+    // Falling 下落
+    float progressFalling = remap(uProgress, 0.1, 1.0, 0.0, 1.0);
+    progressFalling = clamp(progressFalling, 0.0, 1.0);
+    progressFalling = 1.0 - pow(1.0 - progressFalling, 3.0);
+    newPosition.y -= progressFalling * 0.2 * (1.0 - pow(aDelay * 4.0, 0.5));
+
     float progress = clamp(uProgress - aDelay, 0.0, 1.0);
-
     newPosition = mix(vec3(0.0), newPosition, progress);
-
-    float size = 1.0 - pow(aDelay * 4.0, 2.0);
 
     vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
 
+    float size = 1.0 - pow(aDelay * 4.0, 2.0);
     gl_PointSize = uSize * uResolution.y * uPixelRatio * size;
     gl_PointSize *= 1.0 / -viewPosition.z;
     // Final position
